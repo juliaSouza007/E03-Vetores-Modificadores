c3bc0404814d853f7778277d6955b87c8907601e
juliaSouza007 <juliamss07@gmail.com>
2024-04-08 17:04:42 -0300

    trying to do something in the exercise 3

--- src/Cliente.java
+++ src/Cliente.java
@@ -2,11 +2,53 @@ import java.util.Scanner;
 
 public class Cliente {
 
-    String nome;
-    String cpf;
-    String endereco;
-    int idade;
-    char sexo;
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private int idade;
+    private char sexo;
+
+    //getters
+    public String getNome(){
+        return nome;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+
+    public String getEndereco(){
+        return endereco;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+
+    public char getSexo(){
+        return sexo;
+    }
+
+    //setters
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public void setSexo(char sexo){
+        this.sexo = sexo;
+    }
 
     Scanner input = new Scanner(System.in);
 
